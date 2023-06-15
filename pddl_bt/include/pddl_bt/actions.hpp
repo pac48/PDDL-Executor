25928fa171c294501c52c034765f1f6c7e05d7a4
Paul Gesel <paulgesel@gmail.com>
2023-06-15 19:10:30 -0400

    move actions to own file and get bt from planner

---
+++ pddl_bt/include/pddl_bt/actions.hpp
@@ -0,0 +1,70 @@
+#include "bt_medicine_domain.hpp"
+
+BT::NodeStatus
+detectPerson::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus initMoveToLandmark::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus moveToLandmark::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus InitguidePersonToLandmarkAttempt::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus guidePersonToLandmarkAttempt1::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus guidePersonToLandmarkAttempt2::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus checkGuideToSucceeded1::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus checkGuideToSucceeded2::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus UpdatePersonLoc1::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus UpdatePersonLoc2::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus UpdateSuccess1::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus UpdateSuccess2::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus UpdateSuccess3::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus notifyAutomatedMedicineAt::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus notifyRecordedMedicineAt::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus askCaregiverHelpMedicine1::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
+
+BT::NodeStatus askCaregiverHelpMedicine2::tick_action(const InstantiatedAction & action) {
+    return BT::NodeStatus::SUCCESS;
+}
