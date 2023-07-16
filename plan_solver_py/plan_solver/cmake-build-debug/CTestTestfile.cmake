eaa7ff2311ef9be99fd8b6d36b7f87582aa3fa6a
Paul Gesel <paulgesel@gmail.com>
2023-07-16 17:55:42 -0400

    remove build file

--- plan_solver_py/plan_solver/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/paul/smart_home_ws/src/smart-home/external/pddl_executor/plan_solver_py/plan_solver
-# Build directory: /home/paul/smart_home_ws/src/smart-home/external/pddl_executor/plan_solver_py/plan_solver/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
